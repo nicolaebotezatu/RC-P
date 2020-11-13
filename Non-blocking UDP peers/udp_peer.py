13c13b6f2225fc92762781fe018cee5eead285d1
Niki <vs02rjn@yahoo.com>
2020-11-13 08:09:13 +0200

    Non-blocking UDP peers

---
+++ Non-blocking UDP peers/udp_peer.py
@@ -0,0 +1,58 @@
+import socket
+import sys
+import select
+import threading
+
+def receive_fct():
+    global running
+    contor = 0
+    while running:
+        # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
+        # Stabilim un timeout de 1 secunda
+        r, _, _ = select.select([s], [], [], 1)
+        if not r:
+        	contor = contor + 1
+        else:
+            data, address = s.recvfrom(1024)
+            print("S-a receptionat ", str(data), " de la ", address)
+            print("Contor= ", contor)
+
+# Citire nr port din linia de comanda
+if len(sys.argv) != 4:
+    print("help : ")
+    print("  --sport=numarul_meu_de_port ")
+    print("  --dport=numarul_de_port_al_peer-ului ")
+    print("  --dip=ip-ul_peer-ului ")
+    sys.exit()
+
+for arg in sys.argv:
+    if arg.startswith("--sport"):
+        temp, sport = arg.split("=")
+    elif arg.startswith("--dport"):
+        temp, dport = arg.split("=")
+    elif arg.startswith("--dip"):
+        temp, dip = arg.split("=")
+
+# Creare socket UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+s.bind(('0.0.0.0', int(sport)))
+
+running = True
+
+try:
+    receive_thread = threading.Thread(target=receive_fct)
+    receive_thread.start()
+except:
+    print("Eroare la pornirea thread‐ului")
+    sys.exit()
+
+while True:
+    try:
+        data = input("Trimite: ")
+        s.sendto(bytes(data, encoding="ascii"), (dip, int(dport)))
+    except KeyboardInterrupt:
+        running = False
+        print("Waiting for the thread to close...")
+        receive_thread.join()
+        break
